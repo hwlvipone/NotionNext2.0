bfea25414216172f024202a58e3aa548181d5ae4
hwlvipone <hwlvip@gmail.com>
2024-09-07 22:09:05 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -24,9 +24,9 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '云端奇趣', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '分享各种互联网有趣资源', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '分享各种网盘资源,考公资源,三支一扶,申论真题,影视资源,AI工具,收集各种有趣实用的软件和APP的下载,安装,使用方法,发现一些稀奇古怪的的网站,折腾一些有趣实用的教程,分享小快乐', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.yunduanqiqu.com/', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '云盘资源,免费课程,视频课程,BT种子,磁力链接,高清电影电视剧', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '云盘资源,免费课程,视频课程,BT种子,磁力链接,高清电影电视剧，考公资源,三支一扶,申论真题,影视资源,AI工具', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
